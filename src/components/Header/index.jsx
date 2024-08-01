5444563c87832362217209241eb40a181fa7c68c
JonathanB <86327641+jonborges@users.noreply.github.com>
2024-08-01 19:45:41 -0300

    responsividade

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,14 +8,14 @@ function Header() {
                 <li className={styles.logo}>
                     <img src="/images/logo.png" alt="logotipo da empresa" />
                 </li>
-                <div className={styles.navLinks}>
+                <ul className={styles.navLinks}>
                     <li>
                         <Link to="/">Home</Link>
                     </li>
                     <li>
-                        <a href='https://www.figma.com/design/AsmGjxRO6by8QjTG6W7pIx/Cult-iva?t=OUhRKOC94kwmZO0m-0' target='_blank'>Sobre nós</a>
+                        <a href='https://www.figma.com/design/AsmGjxRO6by8QjTG6W7pIx/Cult-iva?t=OUhRKOC94kwmZO0m-0' target='_blank' rel='noopener noreferrer'>Sobre nós</a>
                     </li>
-                </div>
+                </ul>
             </ul>
         </nav>
     );
